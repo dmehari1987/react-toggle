1f0928244852bf4ad5bc08c5b36dae69852d0fd0
Dani <meharidaniel1111@gmail.com>
2020-04-20 16:34:23 +0200

    toggler react app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Toggle extends Component {
+    
+    constructor (props) {
+        super(props);
+        this.state = {
+            isToggleOn: true,
+        }
+        this.handleToggle = this.handleToggle.bind(this)
+    }
+
+    handleToggle = () => {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }))
+    }
+
+    render() { 
+        return ( 
+            
+        <button className= 'btn btn-primary btn-lg col-md-12' onClick= {this.handleToggle}>
+            { this.state.isToggleOn? 'ON' : 'OFF'}
+        </button>
+         );
+    }
+}
+ 
+export default Toggle;
+
+
+ReactDOM.render(<Toggle />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
